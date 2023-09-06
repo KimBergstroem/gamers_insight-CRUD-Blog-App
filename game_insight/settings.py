1c576904ca616ade12861559173ad626189eeeff
Kim Bergstrom <kimmen_b@hotmail.com>
2023-09-06 17:00:15 +0000

    feat: Configure DEBUG and ALLOWED_HOSTS for development

--- game_insight/settings.py
+++ game_insight/settings.py
@@ -28,11 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+development = os.environ.get('DEVELOPMENT', False)
+DEBUG = development
 
-HEROKU_HOSTNAME = os.environ.get('HEROKU_HOSTNAME')
-ALLOWED_HOSTS = [HEROKU_HOSTNAME, '8000-kimbergstroem-pp4-7oi8scylkjd.ws-eu104.gitpod.io']
-
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
@@ -80,15 +82,15 @@ WSGI_APPLICATION = 'game_insight.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = { 'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) }
-
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = { 'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
