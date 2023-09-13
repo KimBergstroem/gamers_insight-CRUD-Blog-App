37afa2b0f2db24f918e2acab5511a0491c4f546b
Kim Bergstrom <kimmen_b@hotmail.com>
2023-09-13 14:27:23 +0000

    chore: Add missing URL path for post_like func

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('index', views.PostList.as_view(), name='index'),
     path('', views.landing_page, name='landing_page'),
     path('about/', views.about, name='about'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
