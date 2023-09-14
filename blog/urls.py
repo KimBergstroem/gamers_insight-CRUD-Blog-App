d014d63f0a1b1e7b2f775ac20bd553f3467846c1
Kim Bergstrom <kimmen_b@hotmail.com>
2023-09-14 19:18:30 +0000

    revert: moved the previous commit from main urls to app urls.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('index', views.PostList.as_view(), name='index'),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
