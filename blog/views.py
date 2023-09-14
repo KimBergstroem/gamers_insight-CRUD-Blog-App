f1b0e3bea7a1d2509d75b21e27c8da5d792fc740
Kim Bergstrom <kimmen_b@hotmail.com>
2023-09-14 13:43:07 +0000

    feat: add contact us and profile pages with routing and view details

--- blog/views.py
+++ blog/views.py
@@ -85,3 +85,10 @@ def landing_page(request):
 def about(request):
     return render(request, 'about.html')
 
+def contactus(request):
+    return render(request, 'contactus.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
