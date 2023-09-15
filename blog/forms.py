c1c94be25316778cd0c1ce70709cbe74e677a73c
Kim Bergstrom <kimmen_b@hotmail.com>
2023-09-15 19:04:10 +0000

    feat: add profile update form

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,30 @@
-from .models import Comment
+from .models import Comment, UserProfile
 from django import forms
 from django.contrib.auth.models import User
 
 class CommentForm(forms.ModelForm):
+    # Comment form in blog posts
     class Meta:
         model = Comment
         fields = ('body',)
+
+class UserForm(forms.ModelForm):
+    # User form
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+        ]
+
+class ProfileForm(forms.ModelForm):
+    # User profile page form
+    class Meta:
+        model = UserProfile
+        fields = [
+            'bio',
+            'profile_picture',
+            'country',
+        ]
