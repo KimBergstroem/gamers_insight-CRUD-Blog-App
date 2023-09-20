65b3990f5e1804051fbdd877a96e51febc7d4ca4
Kim Bergstrom <kimmen_b@hotmail.com>
2023-09-20 10:40:44 +0000

    enhancement: add comments, code cleanup, and category URLs

--- blog/models.py
+++ blog/models.py
@@ -5,65 +5,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
-class GameCategory(models.Model):
-    name = models.CharField(max_length=200)
-    device = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse('index')
-
-
-# The Post model
-class Post(models.Model):
-    content = models.TextField()
-    excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    title = models.CharField(max_length=200, unique=True)
-    status = models.IntegerField(choices=STATUS, default=1)
-    featured_image = CloudinaryField('image', default='placeholder')
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    category = models.CharField(max_length=200, default='fps')
-
-    class Meta:
-        ordering = ['-created_on']
-
-    def __str__(self):
-        return self.title
-
-    def number_of_likes(self):
-        return self.likes.count()
-    
-    # For be able to update the blogpost and redirect user back to home page
-    def get_absolute_url(self):
-        return reverse('index')
-
-
-# The Comment model
-class Comment(models.Model):  
-    body = models.TextField()
-    email = models.EmailField()
-    name = models.CharField(max_length=80)
-    approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-
-    class Meta:
-        ordering = ['created_on']
-    
-    def __str__(self):
-        return f"Comment {self.body} by {self.name}"
-
-
-# The user profile model
+# ==============================
+# Profile Model
+# ==============================
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
@@ -82,3 +30,65 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+# ==============================
+# Post Model
+# ==============================
+class Post(models.Model):
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True)
+    status = models.IntegerField(choices=STATUS, default=1)
+    featured_image = CloudinaryField('image', default='placeholder')
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    category = models.CharField(max_length=200, default='Unspecified')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+    
+    # For be able to update the blogpost and redirect user back to home page
+    def get_absolute_url(self):
+        return reverse('index')
+
+
+# ==============================
+# Comment Model
+# ==============================
+class Comment(models.Model):  
+    body = models.TextField()
+    email = models.EmailField()
+    name = models.CharField(max_length=80)
+    approved = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+# ==============================
+# Category Model
+# ==============================
+class GameCategory(models.Model):
+    name = models.CharField(max_length=200)
+    device = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('index')
