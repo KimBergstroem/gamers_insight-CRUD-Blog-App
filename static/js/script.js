3a671847ac9d34ba4093a07d54bfcc77c00f9265
Kim Bergstrom <kimmen_b@hotmail.com>
2023-09-29 16:34:53 +0000

    perf: optimize HTML and CSS for improved Lighthouse scores

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+//JavaScript for displaying messages
+setTimeout(function () {
+    let messages = document.getElementById("msg");
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 3000);
