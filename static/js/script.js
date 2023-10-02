3abebca29d3c6517d5755b2dd6613a994702cb93
Kim Bergstrom <kimmen_b@hotmail.com>
2023-10-02 11:02:19 +0000

    style: add tooltip hover effect on like button and comment button

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,7 @@ setTimeout(function () {
     let alert = new bootstrap.Alert(messages);
     alert.close();
 }, 3000);
+
+//Tooltip trigger from bootstrap
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
