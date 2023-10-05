7c000e51aaf2c0600d4e896f37548395b1732ca7
Kim Bergstrom <kimmen_b@hotmail.com>
2023-10-05 20:50:56 +0000

    fix: trying to fix 500 error message while signup

--- game_insight/urls.py
+++ game_insight/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf.urls import url
 from django.conf.urls.static import static
